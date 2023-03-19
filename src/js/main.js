b734886e1a9877d7668f8f3f9430be13d0e8b97b
Daniel <sdafer28@gmail.com>
2023-03-19 14:17:44 +0200

    alternative slider added and called in main

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
 import Slider from "./modules/slider";
-
+import ScrollSlider from "./modules/scrollSlider";
 window.addEventListener("DOMContentLoaded", () => {
-    const pageSlider = new Slider('.page', '.next');
+    // const pageSlider = new Slider('.page', '.next');
 
-    pageSlider.render();
+    // pageSlider.render();
+
+    const pageSlidan = new ScrollSlider({
+        page: '.page',
+        btns: '.next',
+        scrollDir: 'column'
+    });
+    
+    pageSlidan.render();
+    pageSlidan.addNav(pageSlidan.btns, pageSlidan.offset);
+    pageSlidan.addNav(pageSlidan.page.querySelectorAll('.sidecontrol > a'), -pageSlidan.offset);
 });
